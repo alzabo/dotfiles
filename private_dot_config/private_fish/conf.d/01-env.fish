e9aefef72a2b848428fefd02692e5d1ca5d8935f
Ryan White <ryan@alzabo.io>
2024-09-16 23:47:39 -0400

    Update env init

--- private_dot_config/private_fish/conf.d/01-env.fish
+++ private_dot_config/private_fish/conf.d/01-env.fish
@@ -1,1 +1,22 @@
-set -gx EDITOR vim
+set addpaths \
+  ~/bin \
+  # ~/zig-linux-x86_64-0.13.0-dev.75+5c9eb4081 \
+  ~/zig-linux-x86_64-0.12.0 \
+  /usr/local/go/bin \
+  ~/go/bin \
+  ~/.cargo/bin
+
+for p in $addpaths
+  if contains $p $PATH
+    continue
+  end
+  if test -e $p
+    set -ag PATH $p
+  end
+end
+
+if command nvim
+  set -gx EDITOR nvim
+else
+  set -gx EDITOR vim
+end
